8da7161094f7d6d9ee1f8de0347bf75bbdd8d751
lucasbbs <lucasbbs@live.fr>
2021-01-11 21:45:04 -0300

    Commit to try to run it in heroku

--- src/main/java/life/belarusian/personapi/controller/PersonController.java
+++ src/main/java/life/belarusian/personapi/controller/PersonController.java
@@ -1,7 +1,5 @@
 package life.belarusian.personapi.controller;
 
-
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
---
+++ src/main/java/life/belarusian/personapi/enums/PhoneType.java
@@ -0,0 +1,10 @@
+package life.belarusian.personapi.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum PhoneType {
+
+}
